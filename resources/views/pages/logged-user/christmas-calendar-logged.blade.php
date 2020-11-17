be57e07dfc23995220fae86d69c9d53ba4f9be07
unknown <miroslav.nedelchev@dentacoin.com>
2020-11-17 13:09:48 +0200

    replace images

--- resources/views/pages/logged-user/christmas-calendar-logged.blade.php
+++ resources/views/pages/logged-user/christmas-calendar-logged.blade.php
@@ -245,7 +245,7 @@
                 </div>
             </section>
             <section class="presents-list padding-top-70">
-                <p class="fs-22 fs-xs-20 lato-regular padding-bottom-30 text-center">We are picking 10 big winners on January 10, 2020:</p>
+                <p class="fs-22 fs-xs-20 lato-regular padding-bottom-130 text-center">We are picking 10 big winners on January 10, 2020:</p>
                 <img src="/assets/images/christmas-calendar-campaign/dentist-christmas.png" class="dentist" alt="Dentist" itemprop="contentUrl"/>
                 <picture itemscope="" itemtype="http://schema.org/ImageObject" class="presents padding-bottom-30">
                     <img src="/assets/images/christmas-calendar-campaign/holiday-calendar-2020-prizes.png" alt="Presents list" itemprop="contentUrl"/>

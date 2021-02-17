32a63fc0eba789488251bb8f65d32cc332a32fc2
unknown <miroslav.nedelchev@dentacoin.com>
2021-02-17 15:21:24 +0200

    test apple

--- public/assets/libs/apple-login/apple-combined-login.js
+++ public/assets/libs/apple-login/apple-combined-login.js
@@ -70,7 +70,7 @@ $('body').on('click', '.apple-custom-btn', function() {
                     console.log('Fire appleID');
                     const data = await AppleID.auth.signIn();
 
-                    proceedWithAppleLogin(data.authorization.id_token, this_btn, 'desktop', true);
+                    proceedWithAppleLogin(data.authorization.id_token, this_btn, 'desktop', undefined, true);
                 } catch ( error ) {
                     //handle error.
                     console.log(error, 'error');

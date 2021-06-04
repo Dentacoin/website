854c510a89050a09020724416b2892c80fbde95a
unknown <miroslav.nedelchev@dentacoin.com>
2021-06-04 11:26:17 +0300

    test price route

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -12,7 +12,7 @@ class PriceController extends Controller
         //var_dump(json_decode($priceFile)->data{0}->quote->USD->price);
         $array = json_decode($priceFile,true);
         var_dump($array['data'][array_key_first($array['data'])]['quote']['USD']['price']);
-        var_dump(strlen($array['data'][array_key_first($array['data'])]['quote']['USD']['price']));
+        var_dump(number_format($array['data'][array_key_first($array['data'])]['quote']['USD']['price'],strlen($array['data'][array_key_first($array['data'])]['quote']['USD']['price']) - 1));
         //var_dump((float)number_format($array[array_key_first($array)]->quote->USD->price,10));
         //echo (float)$array[array_key_first($array)]->quote->USD->price;
         die();

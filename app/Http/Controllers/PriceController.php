a14e3bc56b5970da83d6c278230733c63cda4b11
unknown <miroslav.nedelchev@dentacoin.com>
2021-06-04 11:16:28 +0300

    test price route

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -11,8 +11,8 @@ class PriceController extends Controller
         $priceFile = file_get_contents(ASSETS . 'jsons' . DS . 'dcn-price.json');
         //var_dump(json_decode($priceFile)->data{0}->quote->USD->price);
         $array = (array)json_decode($priceFile)->data;
-        var_dump($array[array_key_first($array)]->quote);
-        echo (float)$array[array_key_first($array)]->quote->USD->price;
+        var_dump((float)number_format($array[array_key_first($array)]->quote->USD->price,10));
+        //echo (float)$array[array_key_first($array)]->quote->USD->price;
         die();
 
 

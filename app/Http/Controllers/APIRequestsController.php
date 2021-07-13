977998813c84d937cab4c6211fbbf8ba0c4931c0
unknown <miroslav.nedelchev@dentacoin.com>
2021-07-13 11:22:40 +0300

    test dcn minihub

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -618,6 +618,8 @@ class APIRequestsController extends Controller {
 
         $resp = json_decode(curl_exec($curl));
         curl_close($curl);
+        var_dump($resp);
+        die();
 
         if (!empty($resp))   {
             if ($returnAsJson) {

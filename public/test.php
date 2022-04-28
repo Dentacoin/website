ab38941c6da9707e8a398aa345d10f164cd60c55
Miroslav Nedelchev <miroslav.nedelchev@dentacoin.com>
2022-04-28 09:44:13 +0300

    add dummy file

---
+++ public/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<a href="dentacoin-wallet://">Open my app</a>
+<br>
+<br>
+<br>
+<br>
+<a href="dentacoin-wallet://?redirect-to=swap">Open my app with route</a>
+
+</body>
+</html>

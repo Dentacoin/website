9c86beed68477a60ea6a5112f5f7d4adb0adcf77
Miroslav Nedelchev <miroslav.nedelchev@dentacoin.com>
2022-04-29 09:52:56 +0300

    test widget

---
+++ public/test-payment-widget.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <script src="<https://global.transak.com/sdk/v1.1/widget.js>" async></script>
+</head>
+
+<body>
+<script>
+    function launchTransak() {
+        let transak = new TransakSDK.default({
+            apiKey: 'ca55ce43-0421-4c7e-a89b-93ebda818cc8',  // Your API Key
+            environment: 'STAGING', // STAGING/PRODUCTION
+            hostURL: window.location.origin,
+            widgetHeight: '625px',
+            widgetWidth: '500px',
+            // Examples of some of the customization parameters you can pass
+            defaultCryptoCurrency: 'ETH', // Example 'ETH'
+            walletAddress: '0xC0c583DaC82Dd59dCE26e00aE6b7Fd272Ca01bEa', // Your customer's wallet address
+            themeColor: '#00afe1', // App theme color
+            fiatCurrency: 'USD', // If you want to limit fiat selection eg 'USD'
+            email: '', // Your customer's email address
+            redirectURL: ''
+        });
+        transak.init();
+        // To get all the events
+        transak
+            .on(transak.ALL_EVENTS, (data) => {
+                console.log(data)
+            });
+        // This will trigger when the user marks payment is made.
+        transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (orderData) => {
+            console.log(orderData);
+            //transak.close();
+        });
+    }
+    window.onload = function() {
+        launchTransak()
+    }
+</script>
+</body>
+
+</html>

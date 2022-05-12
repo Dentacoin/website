3975cae35e539ec07f8a576df7230a41097e2a25
Miroslav Nedelchev <miroslav.nedelchev@dentacoin.com>
2022-05-12 11:29:59 +0300

    stop website zoom

--- resources/views/transak-payment-widget.blade.php
+++ resources/views/transak-payment-widget.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta name="format-detection" content="telephone=no">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+    <meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, width=device-width, height=device-height, target-densitydpi=device-dpi" />
     <meta charset="utf-8" />
     <title>Transak Payment Widget</title>
     <script src="https://global.transak.com/sdk/v1.1/widget.js" async></script>
